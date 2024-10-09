cc2a9e85391633f51575f30b9bd28dad8a4607f1
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-09 16:21:21 +0200

    added __construct on class Movie

--- classes/Movie.php
+++ classes/Movie.php
@@ -6,4 +6,18 @@ class Movie
     public string $language;
     public int $rate;
     public int $year;
+
+    function __construct(
+        $_title,
+        $_type,
+        $_language,
+        $_rate,
+        $_year,
+    ) {
+        $this->title = $_title;
+        $this->type = $_type;
+        $this->language = $_language;
+        $this->rate = $_rate;
+        $this->year = $_year;
+    }
 }
